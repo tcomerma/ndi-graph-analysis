98da158b447f9696627a35da1b8c08c179e79ad4
Comerma Pare, Antoni <acomerma.q@ccma.cat>
2021-03-14 19:38:21 +0100

    linting

--- ndi-graph-analysis.py
+++ ndi-graph-analysis.py
@@ -20,8 +20,10 @@ def is_float(value):
     except:
         return False
 
+
 def cm_to_inch(value):
     return value/2.54
+
 
 def convert_date_delta(data):
     t = ""
@@ -41,7 +43,7 @@ def convert_date_delta(data):
 
 def convert_text_2_float(data):
     fields = ['Framerate', 'Average_video_bitrate', 'Min_video_delay_recv', 'Avg_video_delay_recv',
-              'Dev_video_delay_recv', 'Max_video_delay_recv', 'Min_video_delay_send','Avg_video_delay_send',
+              'Dev_video_delay_recv', 'Max_video_delay_recv', 'Min_video_delay_send', 'Avg_video_delay_send',
               'Dev_video_delay_send', 'Max_video_delay_send']
     for i in fields:
         for j in data:
@@ -51,13 +53,13 @@ def convert_text_2_float(data):
 def create_graph(data, name):
     time = [i['delta'] for i in data]
     bitrate = [i['Average_video_bitrate'] for i in data]
-    
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4,constrained_layout=True)
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, constrained_layout=True)
     fig.suptitle(f'graphs for {name}. 5 secs/sample')
     fig.set_figheight(cm_to_inch(29.7))
     fig.set_figwidth(cm_to_inch(21))
     # fig.tight_layout()
-    ax1.set(xlabel='time (s)', ylabel='bitrate (Mbps)',title="Video data rate")
+    ax1.set(xlabel='time (s)', ylabel='bitrate (Mbps)', title="Video data rate")
     ax1.grid(True)
     ax1.plot(time, bitrate, label='bitrate')
     ax1.legend()
@@ -65,33 +67,40 @@ def create_graph(data, name):
     Min = [i['Min_video_delay_send'] for i in data]
     Avg = [i['Avg_video_delay_send'] for i in data]
     Max = [i['Max_video_delay_send'] for i in data]
-    Dev_up = [(i['Avg_video_delay_send'] + i['Dev_video_delay_send']) for i in data]
-    Dev_down = [(i['Avg_video_delay_send'] - i['Dev_video_delay_send']) for i in data]
-    ax2.set(xlabel='time (s)', ylabel='time between frames (ms)',title="Video @sender")
+    Dev_up = [(i['Avg_video_delay_send'] + i['Dev_video_delay_send'])
+              for i in data]
+    Dev_down = [(i['Avg_video_delay_send'] - i['Dev_video_delay_send'])
+                for i in data]
+    ax2.set(xlabel='time (s)', ylabel='time between frames (ms)',
+            title="Video @sender")
     ax2.grid(True)
     ax2.plot(time, Max, label='max')
     ax2.plot(time, Avg, label='avg')
     ax2.plot(time, Min, label='min')
-    ax2.fill_between(time, Dev_up, Dev_down, alpha=0.2,label='dev')
+    ax2.fill_between(time, Dev_up, Dev_down, alpha=0.2, label='dev')
     ax2.legend()
 
     Min = [i['Min_video_delay_recv'] for i in data]
     Avg = [i['Avg_video_delay_recv'] for i in data]
     Max = [i['Max_video_delay_recv'] for i in data]
-    Dev_up = [(i['Avg_video_delay_recv'] + i['Dev_video_delay_recv']) for i in data]
-    Dev_down = [(i['Avg_video_delay_recv'] - i['Dev_video_delay_recv']) for i in data]
-    ax3.set(xlabel='time (s)', ylabel='time between frames (ms)',title="Video @receiver")
+    Dev_up = [(i['Avg_video_delay_recv'] + i['Dev_video_delay_recv'])
+              for i in data]
+    Dev_down = [(i['Avg_video_delay_recv'] - i['Dev_video_delay_recv'])
+                for i in data]
+    ax3.set(xlabel='time (s)', ylabel='time between frames (ms)',
+            title="Video @receiver")
     ax3.grid(True)
     ax3.plot(time, Max, label='max')
     ax3.plot(time, Avg, label='avg')
     ax3.plot(time, Min, label='min')
-    ax3.fill_between(time, Dev_up, Dev_down, alpha=0.2,label='dev')
+    ax3.fill_between(time, Dev_up, Dev_down, alpha=0.2, label='dev')
     ax3.legend()
 
     Min = [i['Min_video_delay_recv'] - i['Min_video_delay_send'] for i in data]
     Avg = [i['Avg_video_delay_recv'] - i['Avg_video_delay_send'] for i in data]
     Max = [i['Max_video_delay_recv'] - i['Max_video_delay_send'] for i in data]
-    ax4.set(xlabel='time (s)', ylabel='time between frames (ms)',title="Video @receiver - @ sender")
+    ax4.set(xlabel='time (s)', ylabel='time between frames (ms)',
+            title="Video @receiver - @ sender")
     ax4.grid(True)
     ax4.plot(time, Max, label='max')
     ax4.plot(time, Avg, label='avg')
